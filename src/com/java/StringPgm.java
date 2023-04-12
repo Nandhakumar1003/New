0b6dcf1e178cfbcdf716f1522047f4c8e61791fe
Nandhakumar1003 <nandhakumar2001b@gmail.com>
2023-04-12 22:04:50 +0530

    hii

---
+++ src/com/java/StringPgm.java
@@ -0,0 +1,18 @@
+package com.java;
+
+public class StringPgm {
+
+	public static void main(String[] args) {
+				String s="oops";
+				String rev = "";
+				int length = s.length();
+				System.out.println(length);
+				
+				for (int i=length-1; i>=0;i--) {
+					rev = rev+s.charAt(i);
+				}
+				System.out.println(rev);
+		
+	}
+
+}
