59c3f7f6d659ed616ae865a08f38275ca9e5d16d
Nandhakumar1003 <nandhakumar2001b@gmail.com>
2023-04-12 22:25:34 +0530

    test

--- src/com/java/StringPgm.java
+++ src/com/java/StringPgm.java
@@ -7,6 +7,7 @@ public class StringPgm {
 				String rev = "";
 				int length = s.length();
 				System.out.println(length);
+				System.out.println("1234");
 				
 				for (int i=length-1; i>=0;i--) {
 					rev = rev+s.charAt(i);
